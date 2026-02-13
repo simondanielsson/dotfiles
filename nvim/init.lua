da61e832fe18ba1ee3170f9dc5d00f54e117d03e
simondanielsson <simon.danielsson99@hotmail.com>
2026-02-13 22:27:45 +0100

    Rename configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,27 +1,54 @@
+-- VSCode-embedded Neovim: bail out early
 if vim.g.vscode then
-  print("vscode")
-else
-    -- ordinary Neovim
-  require "core"
-
-  local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
-
-  if custom_init_path then
-    dofile(custom_init_path)
-  end
-
-  require("core.utils").load_mappings()
-
-  local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
-  -- bootstrap lazy.nvim!
-  if not vim.loop.fs_stat(lazypath) then
-    require("core.bootstrap").gen_chadrc_template()
-    require("core.bootstrap").lazy(lazypath)
-  end
-
-  dofile(vim.g.base46_cache .. "defaults")
-  -- set the runtime path (rpt)
-  vim.opt.rtp:prepend(lazypath)
-  require "plugins"
+  return
 end
+
+-- Set leader key before anything else
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Load core options
+require("options")
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Load plugins via lazy.nvim
+-- lazy auto-imports all files under lua/plugins/
+require("lazy").setup("plugins", {
+  defaults = { lazy = true },
+  install = { colorscheme = { "night-owl" } },
+  ui = {
+    icons = {
+      ft = "",
+      lazy = "󰂠 ",
+      loaded = "",
+      not_loaded = "",
+    },
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "2html_plugin", "tohtml", "getscript", "getscriptPlugin",
+        "gzip", "logipat", "netrw", "netrwPlugin", "netrwSettings",
+        "netrwFileHandlers", "matchit", "tar", "tarPlugin",
+        "rrhelper", "spellfile_plugin", "vimball", "vimballPlugin",
+        "zip", "zipPlugin", "rplugin", "synmenu",
+        "optwin", "compiler", "bugreport",
+      },
+    },
+  },
+})
+
+require("keymaps")
+require("autocmds")
+require("lsp")
